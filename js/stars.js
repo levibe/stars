889ae1035d1a451038ad47c2b6295d1a9658ae86
Levi Bucsis <levi@peakxp.com>
2017-01-12 09:59:19 -0800

    Make it all more rich

--- js/stars.js
+++ js/stars.js
@@ -13,10 +13,10 @@ for (i = 0; i < 2000; i++) {
 	star.style.backgroundColor = color.toString()
 
 	// make some stars glow bright
-	if (random(1, 10) <= 1) {
+	if (random(1, 10) <= 3) {
 		var glow = random(1, 50)
 		var intensity = glow * random(1, 16) * 0.01
-		star.style['box-shadow'] = "0 0 " + glow + "px " + intensity + "px " + color.darken(random(1, 10)).toString()
+		star.style['box-shadow'] = "0 0 " + glow + "px " + intensity + "px " + color.darken(random(1, 20)).toString()
 		star.style.filter = "blur(" + random(1, 100) * 0.01 + "px)"
 		star.style.opacity = random(90,100) * .01
 	}
